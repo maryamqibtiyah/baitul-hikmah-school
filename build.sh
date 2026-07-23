9043dae72c0a02736568b3625ae793ea2ab04e7f
maryam qibtiyah <maryamqibtiyah36@gmail.com>
2026-07-23 02:04:58 +0100

    Fix gunicorn installation

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+pip install --upgrade pip
 pip install -r requirements.txt
+pip install gunicorn==23.0.0
 python manage.py collectstatic --no-input
 python manage.py migrate
