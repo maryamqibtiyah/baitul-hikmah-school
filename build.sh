07e1f9070ece153a04c0f3cbd2df2ad69b3a574d
maryam qibtiyah <maryamqibtiyah36@gmail.com>
2026-08-11 16:01:19 +0100

    Final deployment ready

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# Set Django settings module
-export DJANGO_SETTINGS_MODULE=baitulhikmah.settings
-
-# Run Django commands
-python3 manage.py collectstatic --no-input
-python3 manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
