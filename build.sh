cb6f13016a93d37378fc5d98df6cfda85748586e
maryam qibtiyah <maryamqibtiyah36@gmail.com>
2026-08-30 03:04:33 +0100

    Add makemigrations to build script

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@ set -o errexit
 
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# Run migrations
+python manage.py makemigrations core --no-input
+python manage.py migrate --no-input
+
+# Collect static files
 python manage.py collectstatic --no-input
-python manage.py migrate
